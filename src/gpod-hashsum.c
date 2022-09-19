c3e381c3c1eb29c665660ddc25e8052364a84603
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2022-09-19 20:35:01 +0100

    gpod-hashsum: incl streamhash

--- src/gpod-hashsum.c
+++ src/gpod-hashsum.c
@@ -6,6 +6,9 @@
 #include <inttypes.h>
 
 #include <gpod-utils.h>
+#ifdef HAVE_FFMPEG
+#include <gpod-ffmpeg.h>
+#endif
 
 
 int main(int argc, char* argv[])
@@ -17,8 +20,13 @@ int main(int argc, char* argv[])
 	return -1;
     }
 
+#ifdef HAVE_FFMPEG
+    gpod_ff_init();
+#endif
+
     struct gpod_hash_digest  res;
     int  ret;
+    char*  streamhash = NULL;
     int  arg = 1;
     while (arg < argc)
     {
@@ -26,7 +34,11 @@ int main(int argc, char* argv[])
 	memset(&res, 0, sizeof(res));
 
 	ret = gpod_hash_digest_file(&res, path);
-	printf("%-11" PRIu64 "  %s  %s\n", ret == 0 ? res.hash : 0, ret == 0 ? res.digest : "", path);
+#ifdef HAVE_FFMPEG
+	gpod_ff_audio_hash(&streamhash, path);
+#endif
+	printf("%-11" PRIu64 "  %s  %s %s\n", ret == 0 ? res.hash : 0, ret == 0 ? res.digest : "", streamhash ? streamhash : "", path);
+	free(streamhash);
     }
 
     return 0;
