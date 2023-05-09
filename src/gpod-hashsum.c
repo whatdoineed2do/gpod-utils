7a0ffeb84971928d13fc951a34be4ef367450934
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2023-05-09 08:32:08 +0100

    gpod-hashsum: missing usage() message

--- src/gpod-hashsum.c
+++ src/gpod-hashsum.c
@@ -13,8 +13,15 @@ int main(int argc, char* argv[])
 {
     const char* argv0 = basename(argv[0]);
 
-    if (argc == 1) {
-	printf("%s: no files\n", argv0);
+    if (argc == 1 || argc == 2 && (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0)) {
+	printf("usage:  %s: <files>\n"
+	       "\n"
+	       "   reports for each input:\n"
+	       "       file DBJ hash file sha1 sum | audio hash | audio DJB hash\n"
+	       "\n"
+	       "   Idneitfiers used by gpod utils to identify duplicate files.\n"
+	       "\n"
+	       , argv0);
 	return -1;
     }
 
@@ -35,7 +42,10 @@ int main(int argc, char* argv[])
 
 	ret = gpod_hash_digest_file(&res, path);
 	gpod_ff_audio_hash(&streamhash, path, &err);
-	printf("%-11" PRIu64 "  %s   %-11" PRIu32 " %s %s\n", ret == 0 ? res.hash : 0, ret == 0 ? res.digest : "", streamhash ? gpod_djbhash(streamhash) : 0, streamhash ? streamhash : err, path);
+
+	printf("%-11" PRIu64 "  %s   %-11" PRIu32 " %s  %s\n",
+	       ret == 0 ? res.hash : 0, ret == 0 ? res.digest : "",
+	       streamhash ? gpod_djbhash(streamhash) : 0, streamhash ? streamhash : err, path);
 	free(streamhash);
 	free(err);
     }
